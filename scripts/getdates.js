2be0f09f42d6c60c16f98c8d40aefe6f5ce70f67
Your Name <you@example.com>
2026-07-27 21:40:59 +0630

    new design of indexhtml

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,2 @@
-document.getElementById("currentYear").textContent = new Date().getFullYear();
-document.getElementById("lastModified").textContent = document.lastModified;
+document.querySelector("#currentYear").innerHTML = new Date().getFullYear()
+document.querySelector("#lastModified").textContent = document.lastModified
